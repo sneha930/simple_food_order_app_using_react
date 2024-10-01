82a841528b9db6e03a4b9943a7f52b27b8f0da38
sneha <sangaonkarsneha@gmail.com>
2024-10-01 15:52:12 +0530

    lect2

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1", {id :"title"}, "Hello");
+const heading2 = React.createElement("h2", {id : "title"}, "React js");
+
+const container = React.createElement("div", {id : "container"}, [heading, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
+
+
